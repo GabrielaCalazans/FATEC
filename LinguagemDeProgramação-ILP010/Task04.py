eb05e1c114cdf44dbc7a13592dd7716f820545cf
gacalaza <gacalaza@42sp.org.br>
2024-09-03 20:40:43 -0300

    done

--- LinguagemDeProgramação-ILP010/Task04.py
+++ LinguagemDeProgramação-ILP010/Task04.py
@@ -6,7 +6,7 @@
 #    By: gacalaza <gacalaza@student.42sp.org.br     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/09/03 16:03:12 by gacalaza          #+#    #+#              #
-#    Updated: 2024/09/03 19:05:35 by gacalaza         ###   ########.fr        #
+#    Updated: 2024/09/03 20:38:29 by gacalaza         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,14 +22,13 @@ from datetime import datetime
 # − Se o número de lados for superior a 4, escrever POLÍGONO NÃO IDENTIFICADO.
 
 def	ex00():
-	# Lê o número de lados do polígono
+	print("\n\n************EX01 ************\n\n")
 	numero_lados = int(input("Digite o número de lados do polígono: "))
 	
 	if numero_lados < 3:
 		print("NÃO É UM POLÍGONO")
 		return
 	
-	# Lê as medidas dos lados do polígono
 	if numero_lados == 3:
 		lado = float(input("Digite a medida do lado do TRIÂNGULO (em cm): "))
 		# Calcula a área do triângulo equilátero
@@ -57,29 +56,21 @@ def	ex00():
 # ⦁ Faça um algoritmo que retorne ao usuário a sua idade a partir da inserção da sua data
 # de nascimento
 def	calcular_idade(data_nascimento):
-	# Obtém a data atual
 	data_atual = datetime.now()
-	
-	# Calcula a diferença entre o ano atual e o ano de nascimento
+
 	idade = data_atual.year - data_nascimento.year
-	
-	# Verifica se o aniversário já ocorreu este ano; se não, subtrai 1 da idade
 	if (data_atual.month, data_atual.day) < (data_nascimento.month, data_nascimento.day):
 		idade -= 1
 	
 	return idade
 
 def	ex01():
-	# Solicita ao usuário a data de nascimento no formato dia/mês/ano
+	print("\n\n************ EX02 ************\n\n")
+
 	data_nascimento_str = input("Digite sua data de nascimento (dd/mm/aaaa): ")
-	
-	# Converte a string de entrada em um objeto de data
 	data_nascimento = datetime.strptime(data_nascimento_str, "%d/%m/%Y")
 	
-	# Calcula a idade
 	idade = calcular_idade(data_nascimento)
-	
-	# Exibe a idade ao usuário
 	print(f"Você tem {idade} anos.")
 
 
@@ -108,23 +99,19 @@ def calcular_reajuste_salarial(salario_atual):
 	else:
 		percentual_aumento = 5
 	
-	# Calcula o valor do aumento
 	valor_aumento = salario_atual * (percentual_aumento / 100)
-	
-	# Calcula o novo salário após o aumento
 	novo_salario = salario_atual + valor_aumento
 	
 	return percentual_aumento, valor_aumento, novo_salario
 
 
 def ex02():
-	# Solicita ao usuário o salário atual
+	print("\n\n************ EX02 ************\n\n")
+
 	salario_atual = float(input("Digite o salário atual do colaborador: R$ "))
-	
-	# Calcula o reajuste salarial
+
 	percentual_aumento, valor_aumento, novo_salario = calcular_reajuste_salarial(salario_atual)
 	
-	# Exibe as informações do reajuste
 	print(f"Salário antes do reajuste: R$ {salario_atual:.2f}")
 	print(f"Percentual de aumento aplicado: {percentual_aumento}%")
 	print(f"Valor do aumento: R$ {valor_aumento:.2f}")
@@ -137,16 +124,13 @@ def ex02():
 # a idade da sua mãe
 # minha mãe é <anos> mais velha do que eu
 def ex03():
-	# Solicita ao usuário sua idade
+	print("\n\n************ EX03 ************\n\n")
+
 	idade_usuario = int(input("Digite a sua idade: "))
-	
-	# Solicita ao usuário a idade da sua mãe
 	idade_mae = int(input("Digite a idade da sua mãe: "))
 	
-	# Calcula a diferença de idade
 	diferenca_idade = idade_mae - idade_usuario
 	
-	# Exibe as informações
 	print(f"Sua idade: {idade_usuario} anos")
 	print(f"Idade da sua mãe: {idade_mae} anos")
 	print(f"Minha mãe é {diferenca_idade} anos mais velha do que eu")
@@ -161,26 +145,23 @@ def ex03():
 # 4.Divisão
 # O usuário deve informar dois números e escolher qual operação deseja fazer de acordo com a lista acima.
 def ex04():
-	# Solicita ao usuário que escolha uma operação
+	print("\n\n************ EX04 ************\n\n")
+
 	print("Escolha a operação desejada:")
 	print("1. Soma")
 	print("2. Subtração")
 	print("3. Multiplicação")
 	print("4. Divisão")
 	
-	# Entrada da escolha do usuário
 	escolha = input("Digite o número correspondente à operação (1/2/3/4): ")
 
-	# Verifica se a escolha é válida
 	if escolha not in ['1', '2', '3', '4']:
 		print("Escolha inválida. Por favor, escolha uma operação entre 1 e 4.")
 		return
 	
-	# Solicita ao usuário dois números para realizar a operação
 	numero1 = float(input("Digite o primeiro número: "))
 	numero2 = float(input("Digite o segundo número: "))
 
-	# Realiza a operação escolhida
 	if escolha == '1':
 		resultado = numero1 + numero2
 		print(f"Soma: {numero1} + {numero2} = {resultado}")
@@ -211,7 +192,8 @@ def ex04():
 # entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado
 # como "Inocente".
 def	ex05():
-	# Lista de perguntas
+	print("\n\n************ EX05 ************\n\n")
+
 	perguntas = [
 		"Telefonou para a vítima?",
 		"Esteve no local do crime?",
@@ -219,17 +201,14 @@ def	ex05():
 		"Devia para a vítima?",
 		"Já trabalhou com a vítima?"
 	]
-	
-	# Contador de respostas afirmativas
+
 	respostas_positivas = 0
 	
-	# Percorre as perguntas e solicita a resposta do usuário
 	for pergunta in perguntas:
 		resposta = input(pergunta + " (sim/não): ").strip().lower()
 		if resposta == "sim":
 			respostas_positivas += 1
-	
-	# Classificação baseada nas respostas afirmativas
+
 	if respostas_positivas == 2:
 		classificacao = "Suspeita"
 	elif 3 <= respostas_positivas <= 4:
@@ -238,8 +217,7 @@ def	ex05():
 		classificacao = "Assassino"
 	else:
 		classificacao = "Inocente"
-	
-	# Exibe a classificação final
+
 	print(f"\nClassificação: {classificacao}")
 
 
@@ -254,36 +232,18 @@ def	ex05():
 # ⦁ Caso o login esteja correto, coloque a mensagem “Bem-vindo (nome da pessoa)”,
 # caso contrário “Login inválido”
 def	ex06():
-	# Definição do login e senha corretos
+	print("\n\n************ EX06 ************\n\n")
+
 	login_correto = "Gabriela"  # Use seu nome ou qualquer outro valor para o login
 	senha_correta = "123.456.789-00"  # Use seu CPF ou qualquer outro valor para a senha
 
-	# Solicita ao usuário o login e a senha
 	login_usuario = input("Digite seu login: ")
 	senha_usuario = input("Digite sua senha: ")
 
-	# Verifica se o login e a senha estão corretos
 	if login_usuario == login_correto and senha_usuario == senha_correta:
 		print(f"Bem-vindo(a), {login_usuario}!")
 	else:
 		print("Login inválido")
-
-
-# def	ex07():
-# Desenvolva um pequeno programa de login e senha. Para isso, peça ao usuário para
-# digitar o seu login e armazene em uma variável. Em seguida, peça para o usuário digitar
-# a sua senha e armazene em outra variável. Por fim, verifique se o login e a senha são
-# iguais àquele definido por você.
-# Dicas:
-# ⦁ Você pode utilizar para login o seu nome e para a senha o seu CPF
-# ⦁ Você deve declarar essas variáveis e armazenar os valores digitados pelo usuário,
-# em seguida compare com os valores originais utilizando o SE-SENAO
-# ⦁ Caso o login esteja correto, coloque a mensagem “Bem-vindo (nome da pessoa)”,
-# caso contrário “Login inválido”
-# ⦁
-# Leia o código de um determinado produto e mostre sua classificação. Utilize a seguinte
-# tabela como referência:
-
 
 
 # Suponha que você esteja desenvolvendo o sistema de um hotel que deva exibir
@@ -300,53 +260,67 @@ def	ex06():
 # ⦁ Se o usuário escolher C), pergunte a ele qual seria o pedido e qual quarto ele está,
 # depois informe a ele, “O pedido tal (pedido que o usuário fez) está sendo feito,
 # aguarde alguns minutos e será entregue no seu quarto (número do quarto)”
-def	ex08():
-	# Exibe as opções para o usuário
+def	ex07():
+	print("\n\n************ EX08 ************\n\n")
+
 	print("Bem-vindo ao Hotel XYZ. Por favor, escolha uma opção:")
 	print("A) Fazer Check-in")
 	print("B) Chamar serviço de quarto")
 	print("C) Fazer pedido")
 	
-	# Solicita a escolha do usuário
+
 	escolha = input("Digite a letra correspondente à sua escolha (A/B/C): ").strip().upper()
-	
-	# Verifica a escolha do usuário
+
 	if escolha == "A":
-		# Solicita informações para check-in
 		nome = input("Digite seu nome: ")
 		data_entrada = input("Digite sua data de entrada (DD/MM/AAAA): ")
 		cpf = input("Digite seu CPF: ")
-		
-		# Exibe mensagem de boas-vindas com o número do quarto (número fictício para o exemplo)
 		numero_quarto = 101
 		print(f"Bem-vindo(a), {nome}! Seu quarto é o número {numero_quarto}.")
 	
 	elif escolha == "B":
-		# Solicita o número do quarto para o serviço de quarto
 		numero_quarto = input("Digite o número do seu quarto: ")
-		
-		# Exibe mensagem de confirmação do serviço de quarto
 		print(f"O serviço de quarto está sendo enviado para a acomodação número {numero_quarto}.")
 	
 	elif escolha == "C":
-		# Solicita o pedido e o número do quarto
 		pedido = input("Qual seria o seu pedido? ")
 		numero_quarto = input("Digite o número do seu quarto: ")
-		
-		# Exibe mensagem de confirmação do pedido
 		print(f"O pedido '{pedido}' está sendo feito. Aguarde alguns minutos e será entregue no seu quarto {numero_quarto}.")
 	
 	else:
-		# Mensagem de erro para escolha inválida
 		print("Escolha inválida. Por favor, selecione uma das opções disponíveis (A, B ou C).")
 
 
 
+# Leia o código de um determinado produto e mostre sua classificação. Utilize a seguinte
+# tabela como referência:
+def	ex08():
+	print("\n\n************ EX08 ************\n\n")
+	codigo = int(input("Digite o código do produto: (número de 1 a 15)"))
+	
+	if codigo == 1:
+		classificacao = "Alimento não-perecível"
+	elif codigo in [2, 3, 4]:
+		classificacao = "Alimento perecível"
+	elif codigo in [5, 6]:
+		classificacao = "Vestuário"
+	elif codigo == 7:
+		classificacao = "Higiene Pessoal"
+	elif 8 <= codigo <= 15:
+		classificacao = "Limpeza e Utensílios Domésticos"
+	else:
+		classificacao = "Código inválido"
+	
+	print(f"A classificação do produto é: {classificacao}")
+
+
+
+# Faça um algoritmo que leia um número e exiba o dia correspondente da semana. (1-
+# Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
 def	ex09():
-	# Solicita ao usuário que insira um número
+	print("\n\n************ EX09 ************\n\n")
 	numero = int(input("Digite um número de 1 a 7 para saber o dia correspondente da semana: "))
 	
-	# Verifica qual dia da semana corresponde ao número inserido
 	if numero == 1:
 		print("1 - Domingo")
 	elif numero == 2:
@@ -365,12 +339,14 @@ def	ex09():
 		print("Valor inválido")
 
 
-
+# Crie um algoritmo que de acordo com a entrada do usuário, ele retorna o gênero de
+# acordo com as seguintes informações: Escolha a letra correspondente: F-Feminino, M-
+# Masculino, T-Transgênero, N-Não Binário, P-Pangênero, A-Agênero. Pergunte o nome
+# do usuário e retorne: “Seu nome é ________ e seu gênero é __________”
 def	ex10():
-	# Solicita o nome do usuário
+	print("\n\n************ EX10 ************\n\n")
 	nome = input("Digite seu nome: ")
-	
-	# Exibe as opções de gênero
+
 	print("Escolha a letra correspondente ao seu gênero:")
 	print("F - Feminino")
 	print("M - Masculino")
@@ -379,10 +355,8 @@ def	ex10():
 	print("P - Pangênero")
 	print("A - Agênero")
 	
-	# Solicita ao usuário que escolha um gênero
 	genero = input("Digite a letra correspondente ao seu gênero: ").strip().upper()
 	
-	# Determina o gênero correspondente
 	if genero == "F":
 		genero_descricao = "Feminino"
 	elif genero == "M":
@@ -398,11 +372,23 @@ def	ex10():
 	else:
 		genero_descricao = "Indefinido"
 	
-	# Exibe a mensagem final com o nome e o gênero
 	print(f"Seu nome é {nome} e seu gênero é {genero_descricao}.")
 
-def ex10():
-	# Exibe a lista de países com números correspondentes
+
+
+# Faça um algoritmo que ao escolher o numero do país ele retorna qual idioma é falado
+# no local de acordo com a tabela abaixo:
+# País
+# Idioma
+# Brasil Português
+# China Mandarim
+# Argentina Espanhol
+# Estados Unidos Inglês
+# Portugal Português
+# Japão Japonês
+def ex11():
+	print("\n\n************ EX11 ************\n\n")
+
 	print("Escolha o número do país para saber o idioma falado:")
 	print("1 - Brasil")
 	print("2 - China")
@@ -411,10 +397,8 @@ def ex10():
 	print("5 - Portugal")
 	print("6 - Japão")
 	
-	# Solicita ao usuário que escolha um número
 	numero = int(input("Digite o número correspondente ao país: "))
 	
-	# Verifica qual idioma corresponde ao número escolhido
 	if numero == 1:
 		idioma = "Português"
 	elif numero == 2:
@@ -430,7 +414,6 @@ def ex10():
 	else:
 		idioma = "Número inválido"
 	
-	# Exibe a mensagem final com o idioma correspondente ou um aviso de número inválido
 	if idioma != "Número inválido":
 		print(f"O idioma falado no país selecionado é: {idioma}.")
 	else:
@@ -445,11 +428,11 @@ def	main():
 	ex04()
 	ex05()
 	ex06()
-	# ex07()
+	ex07()
 	ex08()
 	ex09()
 	ex10()
-
+	ex11()
 
 
 main()
